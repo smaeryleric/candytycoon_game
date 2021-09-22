3c8808b2ce46bb47f4b938c70feee132f1beef85
smaeryleric <msfdragonwar@gmail.com>
2021-09-23 01:34:51 +0500

    in-dev 0003
    Map griding

--- Assets/Scripts/GridBuildingSystem.cs
+++ Assets/Scripts/GridBuildingSystem.cs
@@ -10,6 +10,24 @@ public class GridBuildingSystem : MonoBehaviour
     [SerializeField] private GridLayout _gridLayout;
     [SerializeField] private Tilemap _mainTilemap;
     [SerializeField] private Tilemap _tempTilemap;
+    [SerializeField] private Tile _highlightedTile;
+
+    [SerializeField] private GameObject ObjPrefab;
 
     private void Awake() => Instance = this;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int currentCell = _mainTilemap.WorldToCell(mousePos);
+            _mainTilemap.SetTile(currentCell, _highlightedTile);
+
+            Debug.Log(currentCell);
+            Vector2 cellPos = _mainTilemap.CellToWorld(currentCell);
+            
+            Instantiate(ObjPrefab, new Vector2(cellPos.x, cellPos.y), Quaternion.identity);
+        }
+    }
 }
